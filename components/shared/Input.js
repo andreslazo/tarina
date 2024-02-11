32e35a03b4f4595df52dbf262b00679a2ad44d52
andreslazo <andresmlazo@gmail.com>
2024-02-11 17:25:57 -0300

    Input and text area refactor

--- components/shared/Input.js
+++ components/shared/Input.js
@@ -1,14 +1,11 @@
-export default function Input({name, required, placeholder, onChange }) {
+export default function Input(props) {
 
   return(
     <>
       <input
         type="text"
         className="bg-slate-500 w-full rounded-md p-2 shadow-inner"
-        name={name}
-        required={required}
-        placeholder={placeholder}
-        onChange={onChange}
+        {...props}
       />
     </>
   )
--- components/shared/TextArea.js
+++ components/shared/TextArea.js
@@ -1,15 +1,9 @@
-export default function TextArea({name, required, placeholder, onChange }) {
-
+export default function TextArea(props) {
   return(
-    <>
-      <textarea
-        // eslint-disable-next-line max-len
-        className="bg-slate-300 text-slate-800 h-full w-full rounded-md p-3 shadow-lg resize-none"
-        name={name}
-        required={required}
-        placeholder={placeholder}
-        onChange={onChange}
-      />
-    </>
+    <textarea
+      // eslint-disable-next-line max-len
+      className="bg-slate-300 text-slate-800 h-full w-full rounded-md p-3 shadow-lg resize-none"
+      {...props}
+    />
   )
 }
