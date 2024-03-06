2a9dc49ecf26fb67a65e6252b6a7f14d9619b933
andreslazo <andresmlazo@gmail.com>
2024-03-06 20:19:18 -0300

    Optimize Next Image use

--- components/auth/GoogleLoginButton.js
+++ components/auth/GoogleLoginButton.js
@@ -1,12 +1,13 @@
-import { useAuthContext } from "@/components/context/AuthContext"
 /* eslint-disable max-len */
+import { useAuthContext } from "@/components/context/AuthContext"
+import Image from "next/image"
 
 export default function GoogleLoginButton() {
   const { googleLogin } = useAuthContext()
 
   return (
-    <button class="px-4 py-2 border flex gap-2 border-slate-200 dark:border-slate-700 rounded-lg text-slate-700 hover:border-slate-400 dark:hover:border-slate-500 hover:text-slate-900 hover:shadow transition duration-150" onClick={googleLogin}>
-        <img class="w-6 h-6" src="https://www.svgrepo.com/show/475656/google-color.svg" loading="lazy" alt="google logo" />
+    <button className="px-4 py-2 border flex gap-2 border-slate-200 dark:border-slate-700 rounded-lg text-slate-700 hover:border-slate-400 dark:hover:border-slate-500 hover:text-slate-900 hover:shadow transition duration-150" onClick={googleLogin}>
+        <Image width={24} height={24} src="https://www.svgrepo.com/show/475656/google-color.svg" alt="Google logo" />
         <span>Login with Google</span>
     </button>
   )
--- components/stories/StoryImage.js
+++ components/stories/StoryImage.js
@@ -1,25 +1,27 @@
 import Image from "next/image"
 
 export default function StoryImage({story}) {
-  return (
-    story.thumbnail ? (
-      <div className="relative h-48 w-full bg-gray-200 rounded">
-        <Image
-          src={story.thumbnail}
-          alt="story-thumbnail"
-          layout="fill"
-          objectFit="cover"
-        />
-      </div>
-    ) : (
-      <div className="relative h-48 w-full bg-gray-200 rounded">
-        <Image
-          src='/placeholder.png'
-          alt="story-thumbnail"
-          layout="fill"
-          objectFit="cover"
-        />
-      </div>
-    )
+  return story.thumbnail ? (
+    <div className="relative h-48 w-full bg-gray-200 rounded">
+      <Image
+        src={story.thumbnail}
+        alt="story-thumbnail"
+        fill
+        sizes="100vw"
+        style={{
+          objectFit: "cover"
+        }} />
+    </div>
+  ) : (
+    <div className="relative h-48 w-full bg-gray-200 rounded">
+      <Image
+        src='/placeholder.png'
+        alt="story-thumbnail"
+        fill
+        sizes="100vw"
+        style={{
+          objectFit: "cover"
+        }} />
+    </div>
   )
 }
